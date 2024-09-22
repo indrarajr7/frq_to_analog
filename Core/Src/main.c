fb5c07f5f42133b70f80161da28e3d2c982ab8f1
Indra <indrarajr77@gmail.com>
2024-09-23 00:14:57 +0530

    Eod sunday 23 sep

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -125,38 +125,65 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 }
 void freq_map() {
 		if(!HAL_GPIO_ReadPin(_500_HZ_GPIO_Port,_500_HZ_Pin)){
-			float volt=((REF_VLT * pps)/500) ;
-			dac_set_val(volt) ;
+			if(pps > 500){
+
+				dac_set_val(3.3) ;
+			}
+			else{
+				float volt=((REF_VLT * pps)/500) ;
+				dac_set_val(volt) ;
+			}
 
 		}
 		else if (!HAL_GPIO_ReadPin(_1000_HZ_GPIO_Port, _1000_HZ_Pin)){
-			float volt=((REF_VLT * pps)/1000) ;
-			dac_set_val(volt) ;
-			pps=0;
+			if(pps > 1000){
+
+				dac_set_val(3.3) ;
+			}
+			else{
+				float volt=((REF_VLT * pps)/1000) ;
+				dac_set_val(volt) ;
+			}
 
 		}
 		else if (!HAL_GPIO_ReadPin(_1500_HZ_GPIO_Port, _1500_HZ_Pin)){
-			float volt=((REF_VLT * pps)/1500) ;
-			dac_set_val(volt) ;
-			pps=0;
+			if(pps > 1500){
+
+				dac_set_val(3.3) ;
+			}
+			else{
+				float volt=((REF_VLT * pps)/1500) ;
+				dac_set_val(volt) ;
+			}
+
 
 		}
 		else if (!HAL_GPIO_ReadPin(_2000_HZ_GPIO_Port, _2000_HZ_Pin)){
-			float volt=((REF_VLT * pps)/2000) ;
-			dac_set_val(volt) ;
-			pps=0;
+			if(pps > 2000){
+
+				dac_set_val(3.3) ;
+			}
+			else{
+				float volt=((REF_VLT * pps)/2000) ;
+				dac_set_val(volt) ;
+			}
 
 		}
 		else if (!HAL_GPIO_ReadPin(_5000_HZ_GPIO_Port, _5000_HZ_Pin)){
-			float volt=((REF_VLT * pps)/5000) ;
-			dac_set_val(volt) ;
-			pps=0;
+			if(pps > 5000){
+
+				dac_set_val(3.3) ;
+			}
+			else{
+				float volt=((REF_VLT * pps)/5000) ;
+				dac_set_val(volt) ;
+			}
 
 		}
 		else if (!HAL_GPIO_ReadPin(CUSTOM_FREQ_GPIO_Port, CUSTOM_FREQ_Pin)){
 			float volt=((REF_VLT * pps)/100) ; /// CHANGE THE VALU OF 100 FOR VARABLE FERQUENCY
 			dac_set_val(volt) ;
-			pps=0;
+
 
 		}
 
@@ -218,7 +245,7 @@ int main(void)
   HAL_I2C_Master_Transmit(&hi2c1, 0xC2 , frame, 2, HAL_MAX_DELAY);
   //HAL_Delay(5000);
   dac_set_val(2.0);
-  dac_set_with_range(1.2 , 1.2);
+ // dac_set_with_range(1.2 , 1.2);
   //HAL_Delay(5000);
 
   while (1)
